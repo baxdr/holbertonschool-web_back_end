f6fbd09d3895d06afe2e19d8516679097fc418f4
Bader <10670@holbertonstudents.com>
2025-07-30 07:41:45 +0300

    task 2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """
-Write a type-annotated function measure_time that takes"""
+Write a type-annotated function measure_time that takes
+"""
 import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
-    """Measures the time it takes to run wait_n."""
+    """Measures the time it takes to run wait_n.
+    """
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
