e033106469cec40db0a0bdefb38ddce1e4d64871
Bader <10670@holbertonstudents.com>
2025-07-30 07:20:23 +0300

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,11 +4,10 @@ Write a type-annotated function wait_n that takes
 """
 import asyncio
 from typing import List
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Waits for n random delays and returns a list of the delays."""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
