a2f310ecefade5846b41dd366b0c487694cf4628
Bader <10670@holbertonstudents.com>
2025-07-30 06:23:23 +0300

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function floor that takes
+ a float n as an argument and returns the floor of n as an integer
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float as an integer."""
+    return int(n)
