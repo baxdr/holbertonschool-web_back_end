15f643ab0df67f4760326626b70412562307e45a
Bader <10670@holbertonstudents.com>
2025-07-30 08:19:19 +0300

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-
+"""
+Write a type-annotated function async_comprehension that takes no arguments"""
 from typing import List
 from importlib import import_module as using
 async_generator = using('0-async_generator').async_generator
