e4a85086408609d7533208987da523c14d441431
Bader <10670@holbertonstudents.com>
2025-07-30 08:07:57 +0300

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 Write an asynchronous generator function async_generator that yields"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields a sequence of random numbers."""
     for _ in range(10):
         await asyncio.sleep(1)
