3e6a0ec4528090085a9df3368a47c68bc3adce4e
Baxdr <10670@holbertonstudents.com>
2025-09-05 19:28:55 +0300

    Provided files

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
-/**
- * This script prompts the user for their name and then displays it.
- * It also handles the end of input gracefully.
- */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
-  if (chunk) {
+  if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
